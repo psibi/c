5c126d721fe8cacdbb547ae69d23a5c0114bd975
Sibi <sibi@psibi.in>
2015-04-05 23:51:26 +0530

    10.c

---
+++ chapter 1/10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() 
+{
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') {
+      putchar('\\');
+      putchar('t');
+    }
+    if (c == '\b') {
+      putchar('\\');
+      putchar('b');
+    }
+    if (c == '\\') {
+      putchar('\\');
+    }
+    putchar(c);
+  }
+}
